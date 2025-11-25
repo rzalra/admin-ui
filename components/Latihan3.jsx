0eb769c8ed7ffeeed9729e0147bfb856cb9b2ecc
rzalra <rizalnxa929@gmail.com>
2025-11-25 22:57:19 +0700

    Update project uts

--- components/Latihan3.jsx
+++ components/Latihan3.jsx
@@ -5,7 +5,7 @@ import PricingCard from './PricingCard.jsx';
 export default function Latihan3() {
   return (
     <section className="p-4 mb-8">
-      <h2 className="text-xl font-bold mb-4 text-gray-700">LATIHAN 3: Pricing Table</h2>
+      <h2 className="text-xl font-bold mb-4 textagray-700">LATIHAN 3: Pricing Table</h2>
 
       <div className="flex flex-col md:flex-row justify-center items-stretch gap-4">
         <PricingCard plan="Basic" price="Rp 50.000" />
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// Lokasi: src/App.jsx
-// (File ini tidak digunakan oleh main.jsx saat ini)
-
 import "./App.css";
 
 function App() {
