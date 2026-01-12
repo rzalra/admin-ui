8511d7729b495904e9bc28d8b5991064499264da
rzalra <rizalnxa929@gmail.com>
2026-01-13 00:36:49 +0700

    Small Details : User Interaction

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -2,12 +2,40 @@ import React from "react";
 import AuthLayout from "../components/Layouts/AuthLayout";
 import FormSignIn from "../components/Fragments/FormSignIn";
 
-function SignIn() {
+const SignInPage = () => {
   return (
-    <AuthLayout>
+    <AuthLayout title="Login" type="login">
+      
+      {/* Branding / Title */}
+      <div className="mb-8 text-center">
+        <h1
+          className="
+            text-3xl font-bold
+            opacity-0 translate-y-4
+            animate-[fadeInUp_0.8s_ease-out_forwards]
+          "
+        >
+          <span className="text-blue-600">Fine</span>{" "}
+          <span className="text-slate-800">Bank</span>
+        </h1>
+
+        <p
+          className="
+            mt-2 text-sm text-slate-500
+            opacity-0
+            animate-[fadeIn_1s_ease-out_forwards]
+            animate-delay-200
+          "
+        >
+          Welcome back, please sign in to your account
+        </p>
+      </div>
+
+      {/* Form */}
       <FormSignIn />
+
     </AuthLayout>
   );
-}
+};
 
-export default SignIn;
+export default SignInPage;
